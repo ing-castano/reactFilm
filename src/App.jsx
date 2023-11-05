c34c80c8acb58045fc58b29ef847056ba6d22c4e
Martin Castaño <castanio.m@gmail.com>
2023-11-05 01:31:24 -0300

    routeo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from 'react';
+import {environment} from "@environments";
+import { RouterProvider } from 'react-router-dom';
+import { appRouter } from './core/routes/app_router';
 
 const App = () => {
   return (
     <>
+    {/*
       <div>App para PELIS</div>
       <h2>PRUEBA TEST</h2>
+    */}
+      <RouterProvider router={appRouter} />
+      <h2>{environment.title}</h2>
     </>
   )
 }
