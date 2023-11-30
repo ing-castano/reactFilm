abf8b0b7d7eb62f35a9dd633cec0a0929891bbe3
Martin Castaño <castanio.m@gmail.com>
2023-11-29 23:31:08 -0300

    env3

--- src/core/datasources/remotes/tmdb/tmdb_config.js
+++ src/core/datasources/remotes/tmdb/tmdb_config.js
@@ -1,7 +1,7 @@
 const API_URL = 'https://api.themoviedb.org/'
 const API_IMG_URL = 'http://image.tmdb.org/t/p/'
 const API_VERSION = '3'
-const API_KEY = import.meta.env.VITE_APP_TMDB_API_TOKEN
+const API_KEY = process.env.VITE_APP_TMDB_API_TOKEN
 
 export default {
     API_URL,
