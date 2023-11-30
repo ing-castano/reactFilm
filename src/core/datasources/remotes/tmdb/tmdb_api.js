f15bd7f3ed5c9a557e05dafd5d05a1b5e27a3fb3
Martin Castaño <castanio.m@gmail.com>
2023-11-30 20:28:28 -0300

    env5

--- src/core/datasources/remotes/tmdb/tmdb_api.js
+++ src/core/datasources/remotes/tmdb/tmdb_api.js
@@ -4,7 +4,7 @@ import config from './tmdb_config'
 export const TMDB_API = axios.create({
     baseURL: `${config.API_URL}/${config.API_VERSION}`,
     headers: {
-        Authorization: `Bearer ${config.API_KEY}`
+        Authorization: `Bearer ${import.meta.env.VITE_APP_TMDB_API_TOKEN}`
     },
     params: {
         language: 'es-AR',
