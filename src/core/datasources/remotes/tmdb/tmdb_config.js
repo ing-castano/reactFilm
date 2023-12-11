fbba8b334ef7b26eaed60ced5cf937868f0cf05d
Martin Castaño <castanio.m@gmail.com>
2023-12-11 16:09:42 -0300

    envTest4

--- src/core/datasources/remotes/tmdb/tmdb_config.js
+++ src/core/datasources/remotes/tmdb/tmdb_config.js
@@ -1,8 +1,8 @@
 export const API_URL = 'https://api.themoviedb.org/'
 export const API_IMG_URL = 'https://image.tmdb.org/t/p/'
 export const API_VERSION = '3'
-export const API_KEY = import.meta.env.VITE_TMDB_API_TOKEN
-//export const API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZTE5MjU2ZWJmNjIxNjg4MDEyYjgzYWE3YTE0YzVmMiIsInN1YiI6IjY1NTBhNjhhZDQ2NTM3MDExYjRkNDQ1OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CLuHvsCyTDEcCnQKrrbVlnYpsKAYdFZtF3rRGDPFhcA'
+//export const API_KEY = import.meta.env.VITE_TMDB_API_TOKEN
+export const API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZTE5MjU2ZWJmNjIxNjg4MDEyYjgzYWE3YTE0YzVmMiIsInN1YiI6IjY1NTBhNjhhZDQ2NTM3MDExYjRkNDQ1OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CLuHvsCyTDEcCnQKrrbVlnYpsKAYdFZtF3rRGDPFhcA'
 
 
 export default {
